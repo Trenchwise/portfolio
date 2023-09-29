cf6b9e98b66dd8b91100f31cbbb32f818a058595
Tanya <tanya@tanyas-mbp.home>
2023-09-29 21:22:24 +0100

    Javascript navbar set up but not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
+
 import "./App.css";
 
 const App = () => {
