25b70790f57ddd1c20fcf02e07ede66638a37d94
Tanya <hi@tanyafrench.com>
2023-11-07 13:00:07 +0000

    Project info pages added, footer added, React Router added but not yet working

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
-import AboutMe from "./components/AboutMe";
+// import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+// import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
@@ -12,8 +14,14 @@ const App = () => {
     <>
       <Header />
       <Intro />
+
+      {/* <Routes> */}
       <Projects />
+      {/* <Route path="projects" element={<Projects />} /> */}
       <Contact />
+      {/* <Route path="contact" element={<Contact />} /> */}
+      {/* </Routes> */}
+      <Footer />
     </>
   );
 };
